2f04967bd7205a1bd2c0bc0c56628d0b9ffa48ac
bogomilmihaylov <bogomilmihaylov@live.com>
2019-04-04 20:00:33 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,29 +14,3 @@ function multiply(x, y) {
 function divide(x, y) {
   return x / y
 }
-var number = 10
-function add5() {
-  number += 5
-}
-function divideBy3() {
-  number /= 3
-}
- 
-divideBy3()
- 
-console.log(number) // 3.333333333335
- 
-add5()
- 
-console.log(number) // 8.333333333335
- 
-// reset number
-number = 10
- 
-add5()
- 
-console.log(number) // 15
- 
-divideBy3()
- 
-console.log(number) // 5
